b656681fa0638602ff355ec18745391837c05a52
lanyuqi <111061452+lanyuqi@users.noreply.github.com>
2023-08-21 03:00:29 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -2,4 +2,9 @@ echo '------------------sign------------------'
 curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.network"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
+# 获取签到信息
+SIGN_MESSAGE=$(curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.network"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"')
+
+# 发送签到信息到Server酱
+curl -X POST "https://sctapi.ftqq.com/${SCKEY}.send" -d "text=GLaDOS签到结果&desp=${SIGN_MESSAGE}"
 
